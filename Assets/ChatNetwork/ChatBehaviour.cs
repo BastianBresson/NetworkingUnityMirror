388716beacf3ada09a69f880bb73a5671618668b
Bastian Bresson <bastianlol@live.dk>
2020-06-25 08:43:25 +0200

    Initial Project

---
+++ Assets/ChatNetwork/ChatBehaviour.cs
@@ -0,0 +1,68 @@
+﻿using Mirror;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatBehaviour : NetworkBehaviour
+{
+    [SerializeField] private GameObject chatUI = default;
+    [SerializeField] private TMP_Text chatText = default;
+    [SerializeField] private TMP_InputField inputField = default;
+
+    private static event Action<string> OnMessage;
+
+    public override void OnStartAuthority()
+    {
+        chatUI.SetActive(true);
+
+        OnMessage += HandleNewMessage;
+    }
+
+    [ClientCallback]
+    private void OnDestroy()
+    {
+        if (!hasAuthority) { return; }
+
+        OnMessage -= HandleNewMessage;
+    }
+
+
+    private void HandleNewMessage(string message)
+    {
+        chatText.text += message;
+    }
+
+    [Client]
+    public void Send()
+    {
+        if (!Input.GetKeyDown(KeyCode.Return)) { return; }
+
+        string message = inputField.text;
+
+        if (string.IsNullOrWhiteSpace(message)) { return; }
+
+        CmdSendMessage(message);
+
+        inputField.text = string.Empty;
+    }
+
+    [Command]
+    private void CmdSendMessage(string message)
+    {
+        // validate here
+
+
+        RpcHandleMessage($"{connectionToClient.connectionId}: {message}\n");
+    }
+
+    [ClientRpc]
+    private void RpcHandleMessage(string message)
+    {
+        OnMessage?.Invoke(message);
+    }
+}
+
+
---
+++ Assets/NetworkTest/Player.cs
@@ -0,0 +1,44 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+
+    [SerializeField] private Vector3 movement = new Vector3();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    [Client]
+    void Update()
+    {
+        if (!hasAuthority) { return; }
+
+        if (!Input.GetKeyDown(KeyCode.Space)) { return; }
+
+        CmdMove();
+    }
+
+    [Command]
+    private void CmdMove()
+    {
+        // validate here
+
+        RpcMove();
+    }
+
+    [ClientRpc]
+    private void RpcMove()
+    {
+        transform.Translate(movement);
+    }
+}
+
+
